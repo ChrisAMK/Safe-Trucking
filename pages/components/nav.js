eb54bb6181cf76bdafe166fe349f99d1130a25c8
Chris <chriskl@live.com>
2020-10-07 20:35:02 +0800

    Boilerplate upload

---
+++ pages/components/nav.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+function Navigation() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <h1>HEY</h1>
+      {/* <Link className="navbar-brand" to="/">
+        Google Books
+      </Link>
+      <div>
+        <ul className="navbar-nav nav-links">
+          <li className="nav-item">
+            <Link
+              to="/search"
+            >
+              Search
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/saved">
+              Saved
+            </Link>
+          </li>
+        </ul>
+      </div> */}
+    </nav>
+  )
+}
+
+export default Navigation
